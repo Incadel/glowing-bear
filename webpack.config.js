58bc02e60cfd116a5150e39fc28c82f5cb3d9b0c
Arghyadeep <arghyadeep.k@gmail.com>
2021-07-05 23:36:45 +0530

    Updated host from default to 0.0.0.0

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,7 @@ module.exports = {
     },
     devServer: {
         contentBase: path.resolve(__dirname, 'build'),
+        host: '0.0.0.0',
         port: 2806,
     },
     devtool: 'source-map',
